81fd53e95b623abc6fec9784576dbd9790c9d2b6
p.gabrieldeveloper <paulo.gabriel@atoscapital.com.br>
2022-04-30 12:45:33 -0300

    feat(Created project, adicionando bootstrap) iniciando projeto adicionanco componente eventos, palestrantes e adicionando bootstrap

---
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    PalestrantesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
